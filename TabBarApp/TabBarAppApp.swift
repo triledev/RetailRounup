fd03ad0410091f672950fbac0aa85525b1d29927
Training2021 <gitrainingrepos@gmail.com>
2021-11-19 21:19:13 +0530

    Initial Commit

---
+++ TabBarApp/TabBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarAppApp.swift
+//  TabBarApp
+//
+//  Created by Prabhu Patil on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TabBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
